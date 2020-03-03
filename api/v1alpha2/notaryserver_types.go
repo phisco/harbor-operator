758aad07396518ccc3b4279292f6dc2a09632d96
Pierre Péronnet <pierre.peronnet@ovhcloud.com>
2020-03-03 10:11:47 +0100

    feat(controller) Use graph dependency to manage owned resources
    Signed-off-by: Pierre Péronnet <pierre.peronnet@ovhcloud.com>

---
+++ api/v1alpha2/notaryserver_types.go
@@ -0,0 +1,66 @@
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NotaryServer is the Schema for the registries API
+// +kubebuilder:object:root=true
+// +kubebuilder:storageversion
+// +k8s:openapi-gen=true
+// +resource:path=notaryserver
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="Version",type=string,JSONPath=`.spec.version`,description="The semver Harbor version",priority=5
+// +kubebuilder:printcolumn:name="Replicas",type=string,JSONPath=`.spec.publicURL`,description="The public URL to the Harbor application",priority=0
+// +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.conditions[?(@.statys==true)].type`,description="The current status of the Harbor application",priority=10
+type NotaryServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec NotaryServerSpec `json:"spec,omitempty"`
+
+	// Most recently observed status of the Harbor.
+	Status ComponentStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// NotaryServerList contains a list of NotaryServer
+type NotaryServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NotaryServer `json:"items"`
+}
+
+// NotaryServerSpec defines the desired state of NotaryServer
+type NotaryServerSpec struct {
+	ComponentSpec         `json:",inline"`
+	NotaryServerComponent `json:",inline"`
+
+	// The url exposed to clients to access notary
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Pattern="^https?://.*$"
+	PublicURL string `json:"publicURL"`
+
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Pattern="^https?://.*$"
+	SignerURL string `json:"signerURL"`
+
+	// +kubebuilder:validation:Required
+	DatabaseSecret string `json:"databaseSecret"`
+
+	// +kubebuilder:validation:Required
+	CertificateSecret string `json:"certificateSecret"`
+
+	// +kubebuilder:validation:Required
+	TokenSecret string `json:"tokenSecret"`
+}
+
+// nolint:gochecknoinits
+func init() {
+	SchemeBuilder.Register(&NotaryServer{}, &NotaryServerList{})
+}
